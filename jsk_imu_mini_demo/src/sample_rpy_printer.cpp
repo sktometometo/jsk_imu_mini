b626f0e71e65e88e162804eeb609c158568e8bf8
Koki Shinjo <sktometometo@gmail.com>
2019-04-12 15:52:57 +0900

    [jsk_imu_mini_demo] add sample program and launch file

---
+++ jsk_imu_mini_demo/src/sample_rpy_printer.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+#include "jsk_imu_mini_msgs/Imu.h"
+
+#include <tf/transform_listener.h>
+
+#include <iostream>
+
+void printCallback( const jsk_imu_mini_msgs::Imu::ConstPtr& msg )
+{
+    double r,p,y;
+    tf::Quaternion quat( msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w );
+    tf::Matrix3x3( quat ).getRPY( r, p, y );
+
+    std::cout << "roll  : " << r << std::endl;
+    std::cout << "pitch : " << p << std::endl;
+    std::cout << "yaw   : " << y << std::endl;
+    std::cout << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    if ( argc < 2 ) {
+        std::cout << "usage: sample_quaternion_printer <topicname of jsk_imu_mini_msgs/Imu>" << std::endl;
+        return 0;
+    }
+    std::string topicname_quaternion(argv[1]);
+
+    ros::init( argc, argv, "quaternion_printer" );
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe( topicname_quaternion.c_str(), 10, printCallback );
+
+    ros::spin();
+
+    return 0;
+}
