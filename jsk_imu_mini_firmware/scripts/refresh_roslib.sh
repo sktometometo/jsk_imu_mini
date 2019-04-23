b78140064f548af0b85617eb74d3f7530bc7f3b6
Koki Shinjo <sktometometo@gmail.com>
2019-04-24 01:21:06 +0900

    [jsk_imu_mini_firmware] update refresh scripts

--- jsk_imu_mini_firmware/scripts/refresh_roslib.sh
+++ jsk_imu_mini_firmware/scripts/refresh_roslib.sh
@@ -2,16 +2,32 @@
 
 DIRECTORY_MAIN=$(rospack find jsk_imu_mini_firmware)
 DIRECTORY_ROS_LIB=$DIRECTORY_MAIN/ros_lib/
+DIRECTORY_TEMP=$DIRECTORY_MAIN/temp/
+
+# 一時ディレクトリの作成
+if [ ! -e $DIRECTORY_TEMP ]; then
+    mkdir $DIRECTORY_TEMP
+fi
 
 # 古いバージョンの消去
-if [ -e $DIRECTORY_ROS_LIB ]; then
-    rm -rf $DIRECTORY_ROS_LIB/*
-else
-    mkdir $DIRECTORY_ROS_LIB
+if [ -e $DIRECTORY_ROS_LIB/ros ]; then
+    rm -rf $DIRECTORY_ROS_LIB/ros
+fi
+if [ -e $DIRECTORY_ROS_LIB/jsk_imu_mini_msgs ]; then
+    rm -rf $DIRECTORY_ROS_LIB/jsk_imu_mini_msgs
 fi
 
 # msgファイルなどのbuild
 catkin build jsk_imu_mini_msgs
 
-# headerの生成
-rosrun jsk_imu_mini_firmware make_libraries.py $DIRECTORY_ROS_LIB
+# non-platformbasedなheader類の生成
+rosrun rosserial_client make_libraries $DIRECTORY_TEMP
+
+# 新しいファイル類の移動
+cp -r $DIRECTORY_TEMP/ros_lib/ros $DIRECTORY_ROS_LIB/ros
+cp -r $DIRECTORY_TEMP/ros_lib/jsk_imu_mini_msgs $DIRECTORY_ROS_LIB/jsk_imu_mini_msgs
+
+# 一時ディレクトリの削除
+if [ -e $DIRECTORY_TEMP ]; then
+    rm -rf $DIRECTORY_TEMP
+fi
