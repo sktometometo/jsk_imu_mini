51f2e297f81b05fa99ce13efc59f6164abecd28c
Koki Shinjo <sktometometo@gmail.com>
2019-05-07 12:34:52 +0900

    [jsk_imu_mini_demo] fixed filenames

--- jsk_imu_mini_demo/src/sample_rpy_printer.cpp
+++ jsk_imu_mini_demo/src/sample_rpy_printer.cpp
@@ -20,12 +20,12 @@ void printCallback( const jsk_imu_mini_msgs::Imu::ConstPtr& msg )
 int main(int argc, char **argv)
 {
     if ( argc < 2 ) {
-        std::cout << "usage: sample_quaternion_printer <topicname of jsk_imu_mini_msgs/Imu>" << std::endl;
+        std::cout << "usage: sample_rpy_publisher <topicname of jsk_imu_mini_msgs/Imu>" << std::endl;
         return 0;
     }
     std::string topicname_quaternion(argv[1]);
 
-    ros::init( argc, argv, "quaternion_printer" );
+    ros::init( argc, argv, "rpy_printer" );
     ros::NodeHandle nh;
 
     ros::Subscriber sub = nh.subscribe( topicname_quaternion.c_str(), 10, printCallback );
